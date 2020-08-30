25fed36aae3fe3e91f92e92f3a95e4226c332a14
Dexaran <dexaran820@gmail.com>
2020-08-30 18:13:23 +0400

    Update gamblingdice.cpp

--- src/gamblingdice.cpp
+++ src/gamblingdice.cpp
@@ -50,12 +50,31 @@ ACTION gamblingdice::createroom(name player1, asset stake, uint64_t id) {
 [[eosio::on_notify("eosio.token::transfer")]]
 void deposit(name from, name to, asset quantity, std::string memo) {
   // Memo format:
-  // "room-12 entropy-62024e873202aff4a0466515bfd68208fd1e4353be57d65f032d184f4b24c921"
-  // where `room`-`<number>` is the `id` of a room players wants to play at
-  //       `entropy`-`sha3_hash(<number>)` is a sha3 hash of the number that is used as entropy
+  // "12;62024e873202aff4a0466515bfd68208fd1e4353be57d65f032d184f4b24c921"
+  // where the first number before `;` delimiter is the `id` of a room players wants to play at
+  //       the second hex variable is `sha3_hash(<number>)` is a sha3 hash of the number that is used as entropy
 
   //checksum256 _hash = sha256(memo, 128);
 
+  checksum256 _hash = sha256(memo.c_str(), sizeof(memo));
+
+  std::string user_input = memo;
+
+  std::string delimiter = ";";
+  size_t pos = user_input.find(delimiter);
+  std::string room_id;
+  std::string entropy_hash;
+
+  room_id = user_input.substr(0, pos);
+  user_input.erase(0, pos + delimiter.length());
+
+  pos = user_input.find(delimiter);
+  entropy_hash = user_input.substr(0, pos);
+
+  eosio::print("room_id", room_id);
+  eosio::print("entropy_hash", entropy_hash);
+
+  
 }
 
 EOSIO_DISPATCH(gamblingdice, (hi)(clear)(createroom))
